e139115f500e7ab3123d3e98b05ae55383c24972
maukii <maunokiiskinen@kamk.fi>
2018-11-04 00:56:37 +0200

    Folders and test scripts

---
+++ WGG_2018/Assets/_Scripts/PlayerInteraction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> interactables = new List<GameObject>();
+    [SerializeField] KeyCode actionKey;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(!interactables.Contains(other.gameObject) && other.gameObject.tag != "Player")
+        {
+            interactables.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (interactables.Contains(other.gameObject))
+        {
+            interactables.Remove(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(actionKey) && interactables.Count != 0)
+        {
+            for (int i = 0; i < interactables.Count; i++)
+            {
+                Debug.Log(interactables[i].name);
+            }
+        }
+    }
+
+}
---
+++ WGG_2018/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static bool canMove = true;
+    [SerializeField] float movementSpeed = 3f;
+
+    void FixedUpdate()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(inputH, inputV, 0).normalized;
+
+        if(canMove)
+            transform.Translate(direction * movementSpeed * Time.deltaTime);
+    }
+}
